638fa7ef50f97976fc307e3a1f7bcc6614934e31
Darkos32 <jorgeoliv32@gmail.com>
2022-06-26 11:03:18 -0300

    pré-mudança na iniciarConexão

--- Lab4/processos.py
+++ Lab4/processos.py
@@ -1,3 +1,4 @@
+from glob import glob
 from multiprocessing.dummy import Process
 import time
 import rpyc
@@ -6,15 +7,16 @@ import threading
 from rpyc.utils.server import ThreadedServer
 from random import randint
 LOCK = multiprocessing.Lock()
-MAX_NUMERO_NOS = 5
+MAX_NUMERO_NOS = 2
 SERVER = 'localhost'
 INICIO = 0
 COMECOU_VOTACAO = 1
 AGUARDANDO_RESULTADO = 2
 TERMINADA_ELEICAO = 3
-identificadoresDisponiveis = list(range(0, MAX_NUMERO_NOS*2))
+identificadoresDisponiveis = list(range(0, MAX_NUMERO_NOS))
 nosAtivos = {}
-
+estados = {}
+conexoes = {}
 
 def gerarIndice(maximo):
     return randint(0, maximo-1)
@@ -37,7 +39,7 @@ def gerarIdentificador():
 
 def gerarVizinhos(identificador):
     vizinhos = {}
-    if identificador - 1 > 0:
+    if identificador - 1 >= 0:
         vizinhos[identificador-1] = gerarPorta(identificador-1)
     if identificador + 1 < MAX_NUMERO_NOS:
         vizinhos[identificador+1] = gerarPorta(identificador+1)
@@ -56,104 +58,139 @@ def iniciarConexao(porta):
 
 
 def processo():
+    identificador = gerarIdentificador()
+    numeroVotos = None
+    numEspera = None
+    candidate = identificador
+    estados[identificador] = INICIO
     class No(rpyc.Service):
-        exposed_identificador = gerarIdentificador()
-        porta = gerarPorta(exposed_identificador)
-        vizinhos = gerarVizinhos(exposed_identificador)
-        numeroVotos = None
+        identificacao = identificador
+        porta = gerarPorta(identificacao)
+        vizinhos = gerarVizinhos(identificacao)
+        numVotos =numeroVotos
         lider = None
         exposed_pai = None
-        numeroEspera = None
-        candidato = exposed_identificador
+        numeroEspera = numEspera
+        candidato = candidate
         # jaVotou = False
-        estado = 0
+        # estado = 0
         # jaRecebeuResultado = False
 
-        def on_connect(conn):
+        def on_connect(self,conn):
             return super().on_connect(conn)
 
-        def on_disconnect( conn):
+        def on_disconnect(self,conn):
             return super().on_disconnect(conn)
-        def shouldEcho(self):
-            if self.numeroEspera  == 0:
-                return True
-            else:
-                return False
-        def temPai(self):
-            if self.pai!=None:
-                return True
-            else:
-                return False
-        def sendEcho(self):
-            if self.temPai():
-                        conexao, raiz = inciarConexao(self.vizinhos[self.pai])
-                        rpyc.async_(raiz.echo(self.candidato))
-                        self.estado = AGUARDANDO_RESULTADO
-                        conexao.close()
-            else:
-                self.estado = TERMINADA_ELEICAO
 
-        def exposed_probe(self,pai):
-            if (self.estado != INICIO):
-                self.estado = COMECOU_VOTACAO
+        def shouldEcho(self):
+            if self.numeroEspera == 0:
+                return True
+            else:
+                return False
+
+        def temPai(self):
+            if self.pai != None:
+                return True
+            else:
+                return False
+
+        def sendEcho(self):
+            global estados
+            if self.temPai():
+                conexao, raiz = iniciarConexao(self.vizinhos[self.pai])
+                rpyc.async_(raiz.echo(self.candidato))
+                estados[self.identificacao] = AGUARDANDO_RESULTADO
+                conexao.close()
+            else:
+                estados[self.identificacao] = TERMINADA_ELEICAO
+
+        def exposed_probe(self, pai):
+            global estados
+            global conexoes
+            if (estados[self.identificacao] == INICIO):
+                estados[self.identificacao] = COMECOU_VOTACAO
                 self.pai = pai
-                self.numeroEspera = len(self.vizinhos)  - 1 if self.temPai() else len(self.vizinhos)
-                for endereco in self.vizinhos.values()
+                self.numeroEspera = len(self.vizinhos) - \
+                    1 if self.temPai() else len(self.vizinhos)
+                print("no " + str(self.identificacao) + "espera " + str(self.numeroEspera))
+                conexoes[self.identificacao] = []
+                for endereco in self.vizinhos.values():
                     try:
-                        conexao, raiz =  iniciarConexao(endereco)
-                        raiz.exposed_pai = self.exposed_identificador
-                        rpyc.async_(raiz.probe())
+                        conexao, raiz = iniciarConexao(endereco)
+                        conexoes[self.identificacao].append(conexao)
+                        #raiz.exposed_pai = self.identificacao
+                        #rpyc.async_(raiz.probe(self.identificacao))
+                        #raiz.probe(self.identificacao)
                         conexao.close()
-                    except:
-                        self.numeroEspera-=1
+                    except Exception as  e:
+                        print(e.args)
+                        self.numeroEspera -= 1
                 if (self.shouldEcho()):
                     self.sendEcho()
 
             else:
-              if(pai !=None):
-                conexao, raiz  = iniciarConexao(self.vizinhos[self.pai])
-                raiz.ack()
-                conexao.close() 
-                
-        def exposed_echo(self,candidato):
-            self.numeroEspera-=1
+                if(pai != None):
+                    print(self.identificacao)
+                    conexao, raiz = iniciarConexao(self.vizinhos[self.pai])
+                    raiz.ack()
+                    conexao.close()
+
+        def exposed_echo(self, candidato):
+            self.numeroEspera -= 1
             if (candidato > self.candidato):
                 self.candidato = candidato
             if(self.shouldEcho()):
                 self.sendEcho()
-                    
+
         def exposed_ack(self):
-            self.numeroEspera-=1
+            self.numeroEspera -= 1
             if(self.shouldEcho()):
                 self.sendEcho()
-        def exposed_divulgarResultado(self,vencedor):
+
+        def exposed_divulgarResultado(self, vencedor):
             self.lider = vencedor
             for endereco in self.vizinhos.values():
                 conexao, raiz = iniciarConexao(endereco)
                 raiz.divulgarResultado(vencedor)
                 conexao.close()
-        def exposed_iniciarEleicao(self)
-            self.exposed_probe()
-            while self.estado != TERMINOU_ELEICAO:
-                time.sleep(0.5)
+
+        def exposed_iniciarEleicao(self):
+            global estados
+            self.exposed_probe(None)
+            while estados[self.identificacao] != TERMINADA_ELEICAO:
+                pass #time.sleep(0.5)
             self.exposed_divulgarResultado(self.candidato)
+
         def exposed_mostrarLider(self):
-            existeLider =  True if lider!=None else False
+            existeLider = True if self.lider != None else False
             if(existeLider):
-                print("Nó " + self.exposed_identificador + "diz que Nó " + self.lider +" é o líder"
+                print("Nó " + str(self.identificacao) +
+                      "diz que Nó " + str(self.lider) + " é o líder")
             else:
                 self.exposed_iniciarEleicao()
-            
-            
-            
-    server = threading.Thread(target=iniciarServidor, args=(No, No.porta))
-    server.start()  
-    server.join()
+                print("Nó " + str(self.identificacao) +
+                      " diz que Nó " + str(self.lider) + " é o líder")
+
+    # server = threading.Thread(target=iniciarServidor, args=(No, No.porta,))
+    # server.start()
+    # server.join()
+    #iniciarServidor(No,No.porta)
+    x = No.porta
+    srv = ThreadedServer(No, port=No.porta)
+    srv.start()
+
 
 
 if __name__ == "__main__":
-    for i in range(0, MAX_NUMERO_NOS)
+    for i in range(0, MAX_NUMERO_NOS):
         p = Process(target=processo)
-        time.sleep(5)
+        #time.sleep(5)
         p.start()
+    while True:
+        noAlvo = input()
+        if noAlvo == "fim":
+            break
+        noAlvo = int(noAlvo)
+        conexao, raiz =iniciarConexao(6000 + noAlvo)
+        raiz.mostrarLider()
     p.join()
---
+++ Lab4/utils.py
@@ -0,0 +1,1 @@
+MAX_NUMERO_NOS = 1
